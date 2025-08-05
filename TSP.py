4daab899639cabafd053ab123f21ab106f873ac4
Kai-Ref <555kaireffert@gmail.com>
2025-08-05 03:04:46 +0200

    numpy representation for TSP

--- TSP.py
+++ TSP.py
@@ -1,10 +1,18 @@
-class TSPFileReader:
-    def __init__(self, filepath):
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+class TSP:
+    def __init__(self, filepath, distance_metric='euclidean', precompute_distances=True):
         self.filepath = filepath
         self.metadata = {}
-        self.node_coords = {}
+        self.node_coords = None  # Will be a NumPy array
+        self.distance_metric = distance_metric
+        self.precompute_distances = precompute_distances
+        self.distance_matrix = None
 
     def read(self):
+        coords_list = []
         with open(self.filepath, 'r') as file:
             lines = file.readlines()
 
@@ -23,14 +31,18 @@ class TSPFileReader:
             if in_node_section:
                 parts = line.split()
                 if len(parts) >= 3:
-                    node_id = int(parts[0])
                     x = float(parts[1])
                     y = float(parts[2])
-                    self.node_coords[node_id] = (x, y)
+                    coords_list.append((x, y))
             else:
                 if ':' in line:
                     key, value = line.split(':', 1)
                     self.metadata[key.strip()] = value.strip()
+
+        self.node_coords = np.array(coords_list)
+
+        if self.precompute_distances:
+            self.distance_matrix = cdist(self.node_coords, self.node_coords, metric=self.distance_metric)
 
     def get_metadata(self):
         return self.metadata
@@ -38,16 +50,30 @@ class TSPFileReader:
     def get_node_coords(self):
         return self.node_coords
 
+    def get_distance_matrix(self):
+        if self.distance_matrix is None:
+            self.distance_matrix = cdist(self.node_coords, self.node_coords, metric=self.distance_metric)
+        return self.distance_matrix
+
+    def distance(self, i, j):
+        """Compute distance between node i and j (0-based index)."""
+        if self.distance_matrix is not None:
+            return self.distance_matrix[i, j]
+        else:
+            return cdist([self.node_coords[i]], [self.node_coords[j]], metric=self.distance_metric)[0, 0]
+
     def __repr__(self):
-        return f"<TSPFileReader {self.filepath}, {len(self.node_coords)} nodes>"
+        n = len(self.node_coords) if self.node_coords is not None else 0
+        return f"<TSP {self.filepath}, {n} nodes, metric={self.distance_metric}>"
+
 
 
 
 
 if __name__ == "__main__":
-    reader = TSPFileReader('C:/Users/555ka/Coding/Evolutionary_comp/datasets/eil51.tsp')
-    reader.read()
+    tsp = TSP('C:/Users/555ka/Coding/Evolutionary_comp/datasets/eil51.tsp', distance_metric='euclidean', precompute_distances=True)
+    tsp.read()
 
-    print(reader.get_metadata())
-    print(len(reader.get_node_coords()))
-    print(reader.get_node_coords()[1]) 
+    print(tsp.get_metadata())
+    print(tsp.get_node_coords().shape)
+    print("Distance between node 0 and 1:", tsp.distance(0, 1))
