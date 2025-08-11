27c44a775112bdf3238094ae6578730406689fb0
Gabriel-06 <gabrielfive4@gmail.com>
2025-08-11 18:41:54 +0930

    Implement Elitism selection

--- src/operations/selection/FitnessBased.py
+++ src/operations/selection/FitnessBased.py
@@ -1,7 +1,9 @@
 from src.operations.selection.Selection import Selection
 from src.Population import Population
 from typing import override
+
+
 class FitnessBased(Selection):
     @override
-    def __call__(self, population: Population) -> Population:
+    def __call__(self, population: Population, num_to_select: int) -> Population:
         raise NotImplementedError(f"{self.__class__.__name__} is not implemented yet.")
