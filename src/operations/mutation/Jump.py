07808d0a8042c1364f9df4a173a26fc3db6c27f5
st166687 <st166687@stud.uni-stuttgart.de>
2025-08-13 15:12:19 +0930

    implemented jump mutation

--- src/operations/mutation/Jump.py
+++ src/operations/mutation/Jump.py
@@ -5,24 +5,46 @@ import numpy as np
 
 class Jump(Mutation):
     @override
-    def mutate_individual(self, individual: Individual, i: int, j: int) -> Individual:
+    def mutate_individual(self, individual: Individual, i: int, j: int, update_fitness: bool = False) -> Individual:
         n = len(individual.permutation)
         assert 0 <= i < n, "Index i is out of bounds."
         assert 0 <= j < n, "Index j is out of bounds."
         assert i != j, "Indices i and j must be different."
+        
+        # place i in position j and shift rest
+        new_tour = individual.permutation.copy()  # Copy the original tour
+        moved_city = new_tour.pop(i)
+        new_tour.insert(j, moved_city)
 
-        # Create a new tour by removing city at i and inserting it at j
-        new_tour = individual.permutation.copy() # Copy the original tour
-        city = new_tour.pop(i)
-        new_tour.insert(j, city)
-
-        # Removed, since it is cleaner to not overwrite the initial permutation
-        # Update individual's permutation with the mutated tour
-        # individual.permutation = new_tour
-        return Individual(permutation=new_tour, tsp=individual.tsp)
+        # Create a new individual and compute its fitness efficiently
+        new_individual = Individual(permutation=new_tour, tsp=individual.tsp)
+        # if update_fitness as this might not be required for EA
+        new_individual.fitness = self.efficient_fitness_calculation(individual, i, j) if update_fitness else None
+        return new_individual
     
     @override
-    def efficient_fitness_calculation(self, individual: Individual, i: int, j: int) -> None:
-        # detect the edges that were removed/added and compute their difference
-        raise NotImplementedError("Efficient fitness calculation for jump mutation is not implemented yet.")
-    
+    def efficient_fitness_calculation(self, individual, i, j):
+        # Detect the edges that were removed/added and compute their difference
+        n = len(individual.permutation)
+        tsp = individual.tsp
+
+        # Wrap around indices
+        i_prev = (i - 1) % n
+        i_next = (i + 1) % n
+        j_prev = (j - 1) % n
+        j_next = (j + 1) % n
+
+        # Old distances
+        old_distance = tsp.distance(individual.permutation[i_prev], individual.permutation[i]) + \
+                       tsp.distance(individual.permutation[i], individual.permutation[i_next]) + \
+                       tsp.distance(individual.permutation[j_prev], individual.permutation[j]) + \
+                       tsp.distance(individual.permutation[j], individual.permutation[j_next])
+
+        # New distances
+        new_distance = tsp.distance(individual.permutation[i_prev], individual.permutation[j]) + \
+                       tsp.distance(individual.permutation[j], individual.permutation[i_next]) + \
+                       tsp.distance(individual.permutation[j_prev], individual.permutation[i]) + \
+                       tsp.distance(individual.permutation[i], individual.permutation[j_next])
+
+        # Return the fitness difference
+        return individual.fitness + (new_distance - old_distance)
