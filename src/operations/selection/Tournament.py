22c8a73530bd2ff78e6d734fe81176e1894719df
Gabriel-06 <gabrielfive4@gmail.com>
2025-08-21 12:01:08 +0930

    Added tournament selection with fixed tournament size

--- src/operations/selection/Tournament.py
+++ src/operations/selection/Tournament.py
@@ -1,9 +1,19 @@
 from src.operations.selection.Selection import Selection
 from src.Population import Population
 from typing import override
+import random
 
 
 class Tournament(Selection):
     @override
     def __call__(self, population: Population, num_to_select: int) -> Population:
-        raise NotImplementedError(f"{self.__class__.__name__} is not implemented yet.")
+        new_population = Population(population_size=num_to_select,
+                                    number_of_nodes=population.individuals[0].permutation.size)
+
+        for _ in range(num_to_select):
+            competitors = random.sample(population.individuals, 2)
+
+            winner = max(competitors, key=lambda ind: ind.fitness)
+            new_population.individuals.append(winner)
+
+        return new_population
