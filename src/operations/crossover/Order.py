added fitness calculation, fixed child functionality, bug fixes

src/operations/crossover/Order.py
@@ -23,6 +23,9 @@ class Order(Crossover):
         else:
             keep_start = np.random.randint(keep_end)
 
+        #use these for fitness later
+        seq_i, seq_j = keep_start, keep_end
+
         #insertion from parent1 to child
         for i in range(keep_start, keep_end + 1):
             child1_tour[i] = parent1.permutation[i]
@@ -30,42 +33,54 @@ class Order(Crossover):
             added_to_child_1.append(parent1.permutation[i])
             added_to_child_2.append(parent2.permutation[i])
 
-        keep_end += 1
+        keep_end = (keep_end + 1) % parent_size
         #c_free is the next available free slot in child
         c1_free, c2_free = keep_end, keep_end
 
         #loop throught the second parent to fill in gaps
-        while (np.inf in child1_tour):
-            if keep_end >= parent_size:
-                keep_end = 0
-
-            if c1_free >= parent_size:
-                c1_free = 0
-
+        while ((np.inf in child1_tour) or (np.inf in child2_tour)):
             if (parent2.permutation[keep_end] not in added_to_child_1) and (child1_tour[c1_free] == np.inf):
                 child1_tour[c1_free] = parent2[keep_end]
                 added_to_child_1.append(parent2[keep_end])
-                c1_free += 1
+                c1_free = (c1_free + 1) % parent_size
 
             if (parent1.permutation[keep_end] not in added_to_child_2) and (child2_tour[c2_free] == np.inf):
-                child1_tour[c1_free] = parent1.permutation[keep_end]
+                child1_tour[c2_free] = parent1.permutation[keep_end]
                 added_to_child_2.append(parent1.permutation[keep_end])
-                c1_free += 1
+                c2_free = (c2_free + 1) % parent_size
 
-            keep_end += 1
+            keep_end = (keep_end + 1) % parent_size
 
 
         child1 = Individual(parent_size, parent1.tsp)
         child1.permutation = child1_tour.tolist()
-        child1.fitness = self.efficient_fitness_calculation()
+        child1.fitness += child1.fitness + self.efficient_fitness_calculation(child1, parent1, seq_i, seq_j)
 
         child2 = Individual(parent_size, parent2.tsp)
         child2.permutation = child1_tour.tolist()
-        child2.fitness = self.efficient_fitness_calculation()        
+        child2.fitness += child2.fitness + self.efficient_fitness_calculation(child2, parent2, seq_i, seq_j)        
 
         return tuple(child1, child2)
    
     @override
-    def efficient_fitness_calculation(self, individual: Individual) -> None:
-        raise NotImplementedError("Efficient fitness calculation for %1 is not implemented yet.".format(self.__class__.__name__))
+    def efficient_fitness_calculation(self, individual: Individual, parent: Individual, i: int, j:int) -> float:
+        #if our copied section is the parent, don't need to calculate
+        if(i == 0 and j==(n-1)):
+            return 0.0
+
+        #since order crossover keeps a section from it's parent, we need to recalculate all edges out of the kept segment.
+        tsp = individual.tsp
+        old_tour = parent.permutation
+        new_tour = individual.permutation
+        n = len(new_tour)
+        difference = 0
+
+        #re-calculate every edge except for order edges
+        for e in range(n-1):
+            if((e <= i) and (e >= j)):
+                old_distance = tsp.distance(old_tour[e], old_tour[(e+1)%n])
+                new_distance = tsp.distance(new_tour[e], new_tour[(e+1)%n])
+                difference += (new_distance - old_distance)
+
+        return difference
     

src/operations/crossover/PMX.py
@@ -8,8 +8,9 @@ class PMX(Crossover):
     #from the parents but if the parents share values in the slice, PMX includes additional values, then
     #copies the rest from the other parent.
     @override
-    def xover(self, parent1: Individual, parent2: Individual) -> Individual:
-        added_to_child = []
+    def xover(self, parent1: Individual, parent2: Individual) -> tuple:
+        added_to_child1 = []
+        added_to_child2 = []
         parent_size = parent1.permutation.size()
         #creating a new blank array for the child with a junk value
         child1_tour = np.full(parent_size, np.inf)
@@ -23,43 +24,73 @@ class PMX(Crossover):
             keep_start = np.random.randint(keep_end)
 
         #copies of parent 1 and 2 to make the crossover a bit easier
+        p1_subsequence = []
         p2_subsequence = []
 
         #steps 2-5 from the slides. it'll take too long to explain it here...
         #this also probably isn't the most efficient way...
         for i in range(keep_start, keep_end + 1):
             child1_tour[i] = parent1.permutation[i] 
-            added_to_child.append(parent1.permutation[i])
+            child2_tour[i] = parent2.permutation[i]
+            added_to_child1.append(parent1.permutation[i])
+            added_to_child2.append(parent2.permutation[i])
+            p1_subsequence.append(parent1.permutation[i])
             p2_subsequence.append(parent2.permutation[i])
 
-            for j in range(keep_start, keep_end + 1):
-                if parent1.permutation[j] not in p2_subsequence:
-                    offset = np.where(parent2.permutation == parent1.permutation[j])
-                    k = parent2.permutation[j]
-                    while(k in added_to_child):
-                        k = parent2.permutation[np.where(parent1.permutation == k)]
-                    child1_tour[offset] = k
-                    added_to_child.insert(-1, k)
+            for j1 in range(keep_start, keep_end + 1):
+                if parent1.permutation[j1] not in p2_subsequence:
+                    offset = np.where(parent2.permutation == parent1.permutation[j1])
+                    k1 = parent2.permutation[j1]
+                    while(k1 in added_to_child1):
+                        k1 = parent2.permutation[np.where(parent1.permutation == k1)]
+                    child1_tour[offset] = k1
+                    added_to_child1.append(k1)
+
+            for j2 in range(keep_start, keep_end + 1):
+                if parent2.permutation[j2] not in p1_subsequence:
+                    offset = np.where(parent1.permutation == parent2.permutation[j2])
+                    k2 = parent1.permutation[j2]
+                    while(k2 in added_to_child2):
+                        k2 = parent1.permutation[np.where(parent2.permutation == k2)]
+                    child2_tour[offset] = k2
+                    added_to_child2.append(k2)
 
         #step 6 from the slides
         for r in range(0, parent_size):
             if child1_tour[r] == np.inf:
                 child1_tour[r] = parent2.permutation[r]
+            if child2_tour[r] == np.inf:
+                child2_tour[r] = parent1.permutation[r]
 
-        #TODO: now calculate child2
-        
         child1 = Individual(parent_size, parent1.tsp)
         child1.permutation = child1_tour.tolist()
-        child1.fitness = self.efficient_fitness_calculation()
+        child1.fitness += self.efficient_fitness_calculation(child1, parent1, keep_start, keep_end)
 
         child2 = Individual(parent_size, parent2.tsp)
         child2.permutation = child1_tour.tolist()
-        child2.fitness = self.efficient_fitness_calculation()        
+        child2.fitness += self.efficient_fitness_calculation(child2, parent2, keep_start, keep_end)        
 
         return tuple(child1, child2)
 
 
     @override
-    def efficient_fitness_calculation(self, individual: Individual) -> None:
-        raise NotImplementedError("Efficient fitness calculation for %1 is not implemented yet.".format(self.__class__.__name__))
+    def efficient_fitness_calculation(self, individual: Individual, parent: Individual, i: int, j:int) -> float:
+        #if our copied section is the parent, don't need to calculate
+        if(i == 0 and j==(n-1)):
+            return 0.0
+
+        tsp = individual.tsp
+        old_tour = parent.permutation
+        new_tour = individual.permutation
+        n = len(new_tour)
+        difference = 0
+
+        #re-calculate every edge except for edges that were shared
+        for e in range(n-1):
+            if((e <= i) and (e >= j)):
+                old_distance = tsp.distance(old_tour[e], old_tour[(e+1)%n])
+                new_distance = tsp.distance(new_tour[e], new_tour[(e+1)%n])
+                difference += (new_distance - old_distance)
+
+        return difference
     