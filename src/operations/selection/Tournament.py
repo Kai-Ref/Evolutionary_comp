7380a1b90bfe05ebd63bc8b76b5dcd973c0b57b1
Gabriel-06 <gabrielfive4@gmail.com>
2025-08-24 20:04:14 +0930

    docs: added missing comments in Tournament selection

--- src/operations/selection/Tournament.py
+++ src/operations/selection/Tournament.py
@@ -7,12 +7,16 @@ import random
 class Tournament(Selection):
     @override
     def __call__(self, population: Population, num_to_select: int) -> Population:
+        # Create a new population for the selected individuals
         new_population = Population(population_size=num_to_select,
                                     number_of_nodes=population.individuals[0].permutation.size)
 
+        # Repeat the tournament to select the number of individuals
         for _ in range(num_to_select):
+            # Select two random competitors
             competitors = random.sample(population.individuals, 2)
 
+            # Winner is the individual with the highest fitness
             winner = max(competitors, key=lambda ind: ind.fitness)
             new_population.individuals.append(winner)
 
